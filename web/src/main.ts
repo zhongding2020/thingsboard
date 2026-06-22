a39f98baef071a9754d76e70e81a84ce9acb28a2
zhongding2020 <zhongding_2020@qq.com>
2026-06-22 18:41:32 +0800

    feat: add Tailwind CSS v4 with dark mode CSS variables

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import './styles/theme.css'
+import '@/assets/main.css'
 import App from './App.vue'
 import router from './router'
 
